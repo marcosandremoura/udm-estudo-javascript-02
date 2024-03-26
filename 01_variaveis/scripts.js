488b036d525857602d8f951cd5a24f1b16b1f288
marcosandremoura <marcosandremoura@gmail.com>
2024-03-26 12:16:58 -0300

    first commit

---
+++ 01_variaveis/scripts.js
@@ -0,0 +1,53 @@
+
+
+let nome = "Marcos";
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+
+nome = "Marcus";
+console.log(nome);
+
+
+console.log("\n");
+
+
+
+
+
+
+let laranjas = 5;
+console.log(laranjas * laranjas);
+
+
+laranjas = 10;
+console.log(laranjas * laranjas);
+
+
+console.log("\n");
+
+
+
+
+
+
+laranja = 700;
+console.log(laranja);
+
+
+laranja = "laranja";
+console.log(laranja);
+
+
+console.log("\n");
+
+
+
+
+
+
+let um = 1, dois = 2, tres = 3;
+console.log(um + dois + tres);
+
+
+
